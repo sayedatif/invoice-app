1a315123285adcd61fd031b70a2f8a09dd8867d6
Sayed Atif <sayedatif86@gmail.com>
2017-12-21 23:19:52 +0530

    react project, added semantic UI

---
+++ src/Containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button primary>Hello At</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
